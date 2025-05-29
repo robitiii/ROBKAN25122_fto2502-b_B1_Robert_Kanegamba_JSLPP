16bb64fad9dbcf3001a4cc9b6904681ffaeba244
Robert Erady <137341984+robitiii@users.noreply.github.com>
2025-05-29 21:42:03 +0200

    Added element id for delete button.

--- scripts/dom/elements.js
+++ scripts/dom/elements.js
@@ -4,6 +4,7 @@ export const closeDialogBtn = document.getElementById("close-dialog");
 export const taskForm = document.getElementById("task-form");
 export const dialogTitle = document.getElementById("dialog-title");
 export const taskSubmitBtn = document.getElementById("task-submit-btn");
+export const taskDeleteBtn = document.getElementById("task-delete-btn");
 export const columns = {
   todo: document.getElementById("todo-column"),
   doing: document.getElementById("doing-column"),
