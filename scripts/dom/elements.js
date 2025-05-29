8b9d1b3c9c7be1e086b0cba7965201d7c39d6fdd
Robert Erady <137341984+robitiii@users.noreply.github.com>
2025-05-29 14:48:59 +0200

    created element.js file & export key DOM elements &  column references for task management

---
+++ scripts/dom/elements.js
@@ -0,0 +1,11 @@
+export const addTaskBtn = document.getElementById("add-task-btn");
+export const taskDialog = document.getElementById("task-dialog");
+export const closeDialogBtn = document.getElementById("close-dialog");
+export const taskForm = document.getElementById("task-form");
+export const dialogTitle = document.getElementById("dialog-title");
+
+export const columns = {
+  todo: document.getElementById("todo-column"),
+  doing: document.getElementById("doing-column"),
+  done: document.getElementById("done-column"),
+};
